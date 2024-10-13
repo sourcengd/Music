2d0e098205385aa4cda9177d676d49f2af530491
bblks <168915058+bblks@users.noreply.github.com>
2024-10-13 15:25:09 +0300

    Update سجل الجروبات.py

--- ZeMusic/plugins/play/سجل الجروبات.py
+++ ZeMusic/plugins/play/سجل الجروبات.py
@@ -10,6 +10,10 @@ from pyrogram.types import (
 from ZeMusic import app
 from ZeMusic.utils.database import get_served_chats
 from config import OWNER_ID, LOGGER_ID
+import asyncio
+from datetime import datetime, timedelta
+from pyrogram.enums import ChatMemberStatus
+
 
 photo_urls = [
     "https://envs.sh/Wi_.jpg",
@@ -53,7 +57,62 @@ async def welcome_new_member(client: Client, message: Message):
                     caption=f"↢ مرحباً مطوري <a href='tg://user?id={dev_id}'>{name}</a> نورت الشات ياعزيزي🧸",
                     reply_markup=markup
                 )
+    ############################################################################
 
+    chat = await app.get_chat(message.chat.id)
+    chat_name = chat.title  # اسم الجروب
+    chat_photo = chat.photo  # صورة الجروب
+    
+    chat_id = message.chat.id
+    async for member in client.get_chat_members(chat_id):
+        if member.status == ChatMemberStatus.OWNER:  # جلب منشئ المجموعة فقط
+            owner_id = member.user.id
+            owner_name = member.user.first_name
+     
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[[
+                InlineKeyboardButton(f"{owner_name}", url=f"tg://openmessage?user_id={owner_id}")
+            ]]
+    )       
+    for new_member in message.new_chat_members:
+        first_name = new_member.mention  # اسم العضو الجديد
+        username = new_member.username  # يوزر العضو الجديد
+        # الحصول على الوقت الحالي
+        now = datetime.utcnow()  # استخدام الوقت العالمي UTC
+        # تعديل الوقت ليصبح حسب التوقيت اليمني (UTC +3)
+        yemen_time = now + timedelta(hours=3)
+
+        # تنسيق الوقت والتاريخ
+        join_time = yemen_time.strftime("%I:%M %p")  # الوقت بصيغة 12 ساعة مع AM/PM
+        join_date = yemen_time.strftime("%Y/%m/%d")  # التاريخ بصيغة YYYY/MM/DD
+
+        # إنشاء النص الترحيبي
+        welcome_text = f"""
+𝐰𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐭𝐡𝐞 𝐠𝐫𝐨𝐮𝐩.🧸
+
+__{chat_name}__
+
+➥• Welcome  : {first_name} 
+➥• User : @{username if username else 'No username'}  
+➥• time : {join_time}
+➥• date : {join_date}
+"""
+        # التحقق إذا كانت المجموعة تحتوي على صورة
+        if chat_photo:
+            # تنزيل الصورة وحفظها
+            photo_file = await client.download_media(chat_photo.big_file_id)
+
+            # إرسال الصورة مع النص
+            await message.reply_photo(
+                photo=photo_file,  # استخدام مسار الصورة التي تم تنزيلها
+                caption=welcome_text,
+                reply_markup=keyboard
+            )
+        else:
+            # إرسال النص فقط إذا لم تكن هناك صورة
+            await message.reply_text(welcome_text, reply_markup=keyboard)
+
+    ##########################################################################################
     # جزء التعامل مع إضافة البوت إلى مجموعة جديدة
     if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
         added_by = message.from_user.first_name if message.from_user else "مستخدم غير معروف"
@@ -85,4 +144,6 @@ async def welcome_new_member(client: Client, message: Message):
                     [InlineKeyboardButton(f"{added_by}", url=f"tg://openmessage?user_id={added_id}")]
                 ]
             )
-        ) 
+        )
+
+
