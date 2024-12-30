76e4a8fa061d9a99320a09f060b878abc6f363f7
نجد <basharseed126@gmail.com>
2024-12-30 15:37:57 +0300

    Update play.py

--- ZeMusic/utils/inline/play.py
+++ ZeMusic/utils/inline/play.py
@@ -67,7 +67,8 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],[
-            InlineKeyboardButton(text=config.CHANNEL_NAME, url=config.CHANNEL_LINK)],
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"],
+                    callback_data="close",)],
     ]
     return buttons
 
@@ -81,7 +82,8 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],[
-            InlineKeyboardButton(text=config.CHANNEL_NAME, url=config.CHANNEL_LINK)],
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"],
+                    callback_data="close",)],
     ]
     return buttons
 
