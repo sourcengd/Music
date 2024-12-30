b26b4f256846b03b911b1955a4b386248de663f4
نجد <basharseed126@gmail.com>
2024-12-30 15:40:12 +0300

    Update بحث.py

--- ZeMusic/plugins/play/بحث.py
+++ ZeMusic/plugins/play/بحث.py
@@ -87,13 +87,7 @@ async def song_downloader1(client, message: Message):
             performer=info_dict.get("uploader", "Unknown"),
             thumb=thumb_name,
             duration=dur,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(text=config.CHANNEL_NAME, url=lnk),
-                    ],
-                ]
-            ),
+            
         )
         try:
             await app.send_audio(
@@ -104,13 +98,7 @@ async def song_downloader1(client, message: Message):
                 performer=info_dict.get("uploader", "Unknown"),
                 thumb=thumb_name,
                 duration=dur,
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(text=config.CHANNEL_NAME, url=lnk),
-                        ],
-                    ]
-                ),
+                
             )
         except:
             pass
