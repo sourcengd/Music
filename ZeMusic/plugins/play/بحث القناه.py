3d73835fe178ea69c2f097379b89c7314eb69036
نجد <basharseed126@gmail.com>
2025-02-09 15:06:21 +0300

    Update بحث القناه.py

--- ZeMusic/plugins/play/بحث القناه.py
+++ ZeMusic/plugins/play/بحث القناه.py
@@ -1,51 +1,38 @@
 import os
-import re
+import requests
 import config
 import aiohttp
 import aiofiles
-from ZeMusic.platforms.Youtube import cookie_txt_file
 
 import yt_dlp
 from yt_dlp import YoutubeDL
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from youtube_search import YoutubeSearch
 
-from ZeMusic import app
+from ZeMusic import app, YouTube
 from ZeMusic.plugins.play.filters import command
 
 def remove_if_exists(path):
     if os.path.exists(path):
         os.remove(path)
         
-lnk = config.CHANNEL_LINK
+lnk = "https://t.me/" + config.CHANNEL_LINK
 Nem = config.BOT_NAME + " ابحث"
-
-@app.on_message(command(["song", "/song", "بحث", Nem,"يوت"]) & filters.channel)
-async def song_downloader3(client, message: Message):
+@app.on_message(command(["song", "يوت", "بحث", Nem]) & filters.channel)
+async def song_downloader(client, message: Message):
     query = " ".join(message.command[1:])
     m = await message.reply_text("<b>⇜ جـارِ البحث ..</b>")
     
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
-        if not results:
-            await m.edit("- لم يتم العثـور على نتائج حاول مجددا")
-            return
-
         link = f"https://youtube.com{results[0]['url_suffix']}"
         title = results[0]["title"][:40]
-        title_clean = re.sub(r'[\\/*?:"<>|]', "", title)  # تنظيف اسم الملف
         thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"{title_clean}.jpg"
-
-        # تحميل الصورة المصغرة
-        async with aiohttp.ClientSession() as session:
-            async with session.get(thumbnail) as resp:
-                if resp.status == 200:
-                    f = await aiofiles.open(thumb_name, mode='wb')
-                    await f.write(await resp.read())
-                    await f.close()
-
+        thumb_name = f"{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
         duration = results[0]["duration"]
 
     except Exception as e:
@@ -55,57 +42,36 @@ async def song_downloader3(client, message: Message):
     
     await m.edit("<b>جاري التحميل ♪</b>")
     
-    ydl_opts = {
-        "format": "bestaudio[ext=m4a]",  # تحديد صيغة M4A
-        "keepvideo": False,
-        "geo_bypass": True,
-        "outtmpl": f"{title_clean}.%(ext)s",  # استخدام اسم نظيف للملف
-        "quiet": True,
-        "cookiefile": cookie_txt_file(),
-    }
-
     try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(link, download=True)  # التنزيل مباشرة
-            audio_file = ydl.prepare_filename(info_dict)
-
-        # حساب مدة الأغنية
+        audio_file, tre= await YouTube.download(results[0]["id"], None)
+        rep = f"⟡ {app.mention}"
+        host = str(rep)
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
             secmul *= 60
-
-        # إرسال الصوت
+        
         await message.reply_audio(
             audio=audio_file,
-            caption=f"⟡ {app.mention}",
+            caption=rep,
             title=title,
-            performer=info_dict.get("uploader", "Unknown"),
+            performer=host,
             thumb=thumb_name,
             duration=dur,
-            
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text=config.CHANNEL_NAME, url=lnk),
+                    ],
+                ]
+            ),
         )
-
-        try:
-            await app.send_audio(
-                chat_id="@vbbbbnnnm",  # معرف القناة التي تريد الإرسال إليها 
-                audio=audio_file,
-                caption=f"⟡ {app.mention}",
-                title=title,
-                performer=info_dict.get("uploader", "Unknown"),
-                thumb=thumb_name,
-                duration=dur,
-                
-            )
-        except:
-            pass
         await m.delete()
 
     except Exception as e:
-        await m.edit(f"error, wait for bot owner to fix\n\nError: {str(e)}")
+        await m.edit("error, wait for bot owner to fix")
         print(e)
 
-    # حذف الملفات المؤقتة
     try:
         remove_if_exists(audio_file)
         remove_if_exists(thumb_name)
